37fb4162b370abf12acfbc647f824f659b90eae6
Akhil <akhiljohns250@gmail.com>
2023-12-07 15:24:39 +0530

    Basic Nodejs Express Server

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.listen(port,() => {
+  console.log(`<------------------------------------->`)
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`)
+  console.log(`<------------------------------------->`)
+});
