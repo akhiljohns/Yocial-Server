6d2ac0ed0de40ae8d51fd8b8117614e6abab189e
Akhil <akhiljohns250@gmail.com>
2023-12-21 19:33:58 +0530

    Expiry For Email Verification

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 const router = Router();
 
 
-import { sendVerificationEmail } from "../controllers/userController.js";
+import { sendVerificationEmail,verifyEmail } from "../controllers/userController.js";
 
 
 // @desc    Sent email verification
 // @access  Registerd users
 router.post("/send-verification", sendVerificationEmail);
 
-// router.get("/verify/:id/:token", verifyEmail);
+router.get("/verify/:id/:token", verifyEmail);
 
 export default router;
     
