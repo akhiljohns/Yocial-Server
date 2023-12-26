3170b5a1fb4544bd3e42cba45a4354f38882412d
Akhil Johns <akhiljohns250@gmail.com>
2023-12-26 14:04:38 +0530

    Update Post

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-import { createPost } from "../controllers/postController.js";
+import { createPost, updatePost } from "../controllers/postController.js";
 
 // @desc    Create post
 // @access  Authenticated user
 router.post("/create-post", createPost);
+
+// @desc    Update post
+// @access  Authenticated user
+router.post("/update-post/:postId", updatePost);
+
 
 export default router;
