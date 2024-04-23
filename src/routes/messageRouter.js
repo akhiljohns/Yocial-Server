a7a4dd9ffbb1ec3d9bcf98d0bf316a1d3c2f1ae2
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-04-23 14:11:12 +0530

    chat middleware fixed

--- src/routes/messageRouter.js
+++ src/routes/messageRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import protect from "../middleware/authMiddleware.js";
+import protect from "../middlewares/authMiddleware.js";
+
 import {
   getChatRoomWithIds,
   getChats,
@@ -9,41 +10,25 @@ import {
 } from "../controllers/chatController.js";
 const router = Router();
 
-
-
-
-
-
-
-
 // @desc    Get chats from a room
 // @access  Users - private
-router.get("/inbox/:roomId", protect, getChats);
-
+router.get("/inbox/:roomId", getChats);
 
 // @desc    Create or get chatRoom of two
 // @access  Users - private
 router.put("/inbox/room/:firstId/:secondId", setChatRoom);
-                        //@user  //@other
-
+//@user  //@other
 
 // @desc    Get chatRoom of two
 // @access  Users - private
 router.get("/inbox/room/fetch/:firstId/:secondId", protect, getChatRoomWithIds);
 
-
 // @desc    Send new chat
 // @access  Users - private
-router.post("/inbox/new-message/:roomId", protect, sendNewMessage)
-
+router.post("/inbox/new-message/:roomId", protect, sendNewMessage);
 
 // @desc    Get rooms with userID
 // @access  Users - private
-router.get("/inbox/get-room/userID/:userId", protect, getRoomwithUserID)
-
-
-
-
-
+router.get("/inbox/get-room/userID/:userId", protect, getRoomwithUserID);
 
 export default router;
