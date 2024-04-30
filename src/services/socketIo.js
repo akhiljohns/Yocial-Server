9caf31a9499163e45617b92298e3023ce58c82f9
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-04-30 17:18:42 +0530

    fixed socket emit issue

--- src/services/socketIo.js
+++ src/services/socketIo.js
@@ -13,41 +13,15 @@ const socketIo_Config = (io) => {
                 console.log("recieved data in servere socket", socket.id, roomId);
 
                 socket.on("sendMessage", (roomId, message, senderId, cb)=> {
-                    console.log("newMessage from socket",roomId,message, senderId);
-                    // await sendMessageNotification(senderId, roomId, message)
-                        console.log('yoooooo');
-                    cb(message);
+                    cb(message)
 
                     
 
                     io.to(roomId).emit("recieveMessage", message);
                 })
 
-                // socket.on("typing", (typing, senderId, cb)=> {
-
-                //     io.to(roomId).emit("userTyping", {typing, senderId})
-                // })
-
+                
             })
-
-
-            // socket.on("roomSetup", (userId)=> {
-            //     socket.join(userId);
-            //     console.log("socket connection established for setting up rooms");
-
-            //     socket.on("newMessageUpdate", (newChat, cb)=> {
-            //         io.to(userId).emit("updateChatRooms", newChat)
-            //     });
-
-
-            // })
-
-
-            
-
-
-
-
             //disconnecting user
             socket.on("disconnect", ()=>{
                 console.log('User disconnected.');
