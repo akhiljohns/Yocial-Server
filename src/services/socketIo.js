c057744c6327a86da41d8f41a1daacf29a6c1ee5
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-04-30 19:54:16 +0530

    formatted socket.io

--- src/services/socketIo.js
+++ src/services/socketIo.js
@@ -1,40 +1,25 @@
-
 const socketIo_Config = (io) => {
+  try {
+    io.on("connect", (socket) => {
+      //establishing connection
+      console.log("User has been connected.");
 
-    try {
-        
-        io.on("connect", (socket)=> {
-            //establishing connection
-            console.log('User has been connected.');
+      socket.on("newUser", (roomId) => {
+        socket.join(roomId);
+        console.log("recieved data in servere socket", socket.id, roomId);
 
-            socket.on("newUser", (roomId)=> {
-                
-                socket.join(roomId);
-                console.log("recieved data in servere socket", socket.id, roomId);
+        socket.on("sendMessage", (roomId, message, senderId, cb) => {
+          cb(message);
 
-                socket.on("sendMessage", (roomId, message, senderId, cb)=> {
-                    cb(message)
-
-                    
-
-                    io.to(roomId).emit("recieveMessage", message);
-                })
-
-                
-            })
-            //disconnecting user
-            socket.on("disconnect", ()=>{
-                console.log('User disconnected.');
-            })
-        })
-
-
-    } catch (error) {
-        
-    }
-
-
-
+          io.to(roomId).emit("recieveMessage", message);
+        });
+      });
+      //disconnecting user
+      socket.on("disconnect", () => {
+        console.log("User disconnected.");
+      });
+    });
+  } catch (error) {}
 };
 
 export default socketIo_Config;
